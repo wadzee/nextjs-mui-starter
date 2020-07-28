2ea2dc18ce48882650ea4d26005488360db5f358
Radzi <radzi@radziramli.com>
2020-07-28 12:38:51 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// next.config.js
+const nextConfig = {
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]",
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
